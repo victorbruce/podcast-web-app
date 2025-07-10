7aaeeb8c05212b92e7da90735f318cbe5d4cf911
Victor Bruce <victorbruce82@gmail.com>
2025-07-10 13:32:20 +0000

    fix: routing issues on the dashboard

--- src/app/features/admin/admin.routes.ts
+++ src/app/features/admin/admin.routes.ts
@@ -1,5 +1,5 @@
 import { Routes } from '@angular/router';
-import { DashboardComponent } from './dashboard/dashboard.component';
+import { DashboardLayoutComponent } from './dashboard-layout/dashboard-layout.component';
 
 export const adminRoutes: Routes = [
   {
@@ -7,5 +7,33 @@ export const adminRoutes: Routes = [
     redirectTo: 'dashboard',
     pathMatch: 'full',
   },
-  { path: 'dashboard', component: DashboardComponent },
+  {
+    path: 'dashboard',
+    component: DashboardLayoutComponent,
+    children: [
+      {
+        path: '',
+        loadComponent: () =>
+          import('./dashboard/dashboard.component').then((m) => m.DashboardComponent),
+      },
+      {
+        path: 'episodes',
+        loadComponent: () => import('./episode/episode.component').then((m) => m.EpisodeComponent),
+      },
+      {
+        path: 'playlists',
+        loadComponent: () =>
+          import('./playlist/playlist.component').then((m) => m.PlaylistComponent),
+      },
+      {
+        path: 'confessions',
+        loadComponent: () =>
+          import('./confession/confession.component').then((m) => m.ConfessionComponent),
+      },
+      {
+        path: 'teams',
+        loadComponent: () => import('./team/team.component').then((m) => m.TeamComponent),
+      },
+    ],
+  },
 ];
